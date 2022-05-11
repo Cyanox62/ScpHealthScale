acbb295cfcefcab290ce9e26c8cc51a77a808164
Cyanox62 <cyanox62@gmail.com>
2022-05-11 00:33:29 -0400

    Removed test config case

--- ScpHealthScale/Config.cs
+++ ScpHealthScale/Config.cs
@@ -12,9 +12,6 @@ namespace ScpHealthScale
 		public float ScpHealthMultiplier { get; set; } = 1f;
 
 		[Description("Determines the max amount of health each SCP can have with the multiplier.")]
-		public Dictionary<RoleType, int> ScpMaxHealth { get; set; } = new Dictionary<RoleType, int>()
-		{
-			{ RoleType.Scp173, 5000 }
-		};
+		public Dictionary<RoleType, int> ScpMaxHealth { get; set; } = new Dictionary<RoleType, int>();
 	}
 }
